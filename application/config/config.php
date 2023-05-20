2260c67e5c9a4ac766839381411d55986349cc15
bhuvan1997 <133843942+bhuvan1997@users.noreply.github.com>
2023-05-20 13:22:58 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/hacking';
+// $config['base_url'] = 'http://localhost/hacking';
    // $config['base_url'] = 'https://cyberminars.com';
+$config['base_url'] = 'https://php-hackingseekho.azurewebsites.net/';
 
 /*
 |--------------------------------------------------------------------------
